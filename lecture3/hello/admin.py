8cdecce0b2dfe2fbe3fd7013543fa6e9b86cab20
b-isha-l <bishalpoudel607@gmail.com>
2021-03-14 07:32:20 +0545

    1.0

--- lecture3/hello/admin.py
+++ lecture3/hello/admin.py
@@ -1,3 +1,9 @@
+<<<<<<< HEAD
 from django.contrib import admin
 
 # Register your models here.
+=======
+from django.contrib import admin
+
+# Register your models here.
+>>>>>>> ca6d5eb8fe6e74aac7da07683201344a4236911b
